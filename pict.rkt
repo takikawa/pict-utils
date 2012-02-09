8b7a4740cb5df02ab4992e959c5884506501c985
Asumu Takikawa <asumu@ccs.neu.edu>
2012-02-09 13:07:30 -0500

    Improve path macro.

--- pict.rkt
+++ pict.rkt
@@ -1,7 +1,9 @@
 #lang racket
 
 (require slideshow/pict
-         (except-in unstable/gui/ppict grid))
+         racket/draw
+         (except-in unstable/gui/ppict grid)
+         (for-syntax syntax/parse))
 
 ;; grid : nat nat nat [nat] -> pict
 ;; draw a grid
@@ -39,18 +41,31 @@
 ;; path
 ;; pict macro for drawing paths
 ;; e.g.
-;; (path (move-to 0 0)
+;; (path 100 200
+;;       (move-to 0 0)
 ;;       (arc 30 30 0 (degrees->radians 30))
 ;;       (close))
 (define-syntax (path stx)
   (define-syntax-class path-elem
-    (pattern ...))
+    (pattern ((~literal move-to) x y)
+             #:with expr #'(λ (p) (send p move-to x y)))
+    (pattern ((~literal line-to) x y)
+             #:with expr #'(λ (p) (send p line-to x y)))
+    (pattern ((~literal arc) x y w h sr er)
+             #:with expr #'(λ (p) (send p arc x y w h sr er)))
+    (pattern ((~literal close))
+             #:with expr #'(λ (p) (send p close))))
   
   (syntax-parse stx
     [(_ elem:path-elem ...)
      #'(let ()
          (define p (new dc-path%))
-         (elem.expr p) ...)]))
+         (elem.expr p) ...
+         (define-values (x y w h)
+           (send p get-bounding-box))
+         (dc (λ (dc x y)
+             (send dc draw-path p x y))
+           w h))]))
 
 (ppict-do (colorize (grid 300 300 50 1) "gray")
           #:go (coord 0.5 0.5)
@@ -59,5 +74,17 @@
           (vline 1 300)
           #:go (coord 0.5 0.5)
           (circle 200)
-          #:go (coord 0.5 0.5 'lc)
-          (arc 30 30 0 (degrees->radians 30)))
+          #:go (coord 0.5 0.5 'lt)
+          (colorize 
+           (path (move-to 0 0)
+                 (line-to 30 0)
+                 (arc -30 -30 60 60 0 (degrees->radians 30))
+                 (close))
+           "green")
+          #:go (coord 1/2 1/2
+                      #:abs-x (* 100 (cos (degrees->radians 30)))
+                      'cb)
+          (colorize (vline 5 50) "red")
+          #:go (coord 1/2 1/2 'lc)
+          (colorize (hline (* 100 (cos (degrees->radians 30))) 1)
+                    "blue"))
