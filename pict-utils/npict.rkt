cbfaea46a4bfe304202fe1b8b6ce46a3f80b6d67
Asumu Takikawa <asumu@ccs.neu.edu>
2013-03-15 15:42:15 -0400

    Revert "Change how line arguments are given"
    This reverts commit 74a1524e4f170123fdf64273762ef938999223ca.
    
    It turned out to be a bad idea. I could actually
    do this correctly by making the positional arguments
    optional and using a ->i contract, but I no longer
    want to do this.

--- pict-utils/npict.rkt
+++ pict-utils/npict.rkt
@@ -24,11 +24,9 @@
                 #:text string?)
                node?))
   (rename make-line line
-          (->* ((or/c node-name/c pict?)
-                (or/c node-name/c pict?))
-               (#:from (or/c node-name/c pict?)
-                #:to (or/c node-name/c pict?)
-                #:arrow? any/c #:start-angle real? #:end-angle real?
+          (->* (#:from (or/c node-name/c pict?)
+                #:to (or/c node-name/c pict?))
+               (#:arrow? any/c #:start-angle real? #:end-angle real?
                 #:start-align align/c #:end-align align/c
                 #:start-pull real? #:end-pull real? #:line-width real?
                 #:color string? #:under? any/c #:solid? any/c
@@ -89,12 +87,8 @@
               line-width color style under?
               solid?))
 
-(define (make-line _from _to
-                   ;; these two keyword args are here for
-                   ;; backwards compat and for more flexibility
-                   ;; in argument ordering
-                   #:from [from _from]
-                   #:to [to _to]
+(define (make-line #:from from
+                   #:to to
                    #:arrow? [arrow? #f]
                    #:start-angle [start-angle #f]
                    #:end-angle [end-angle #f]
