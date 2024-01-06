fd71fd9aa0a9266ae4f1340b10f5183ef4ecad35
Sam Tobin-Hochstadt <samth@indiana.edu>
2024-01-06 15:23:50 -0500

    fix deps

--- info.rkt
+++ info.rkt
@@ -1,2 +1,14 @@
 #lang info
 (define collection 'multi)
+(define deps '("base"
+               "draw-lib"
+               "plot-gui-lib"
+               "plot-lib"
+               "slideshow-lib"
+               "unstable-lib"))
+(define build-deps '("at-exp-lib"
+                     "pict-doc"
+                     "racket-doc"
+                     "rackunit-lib"
+                     "scribble-lib"
+                     "unstable-contract-lib"))
